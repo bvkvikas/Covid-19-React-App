c1b87736731c1a8f57f39f766acb06f1cf066393
Krishna Vikas <venkatakrishnavika.b@husky.neu.edu>
2020-03-30 17:00:24 -0400

    added linting rules

--- src/components/MainDashboardComponent.js
+++ src/components/MainDashboardComponent.js
@@ -1,53 +1,62 @@
-import React from "react";
-import CountryListComponent from "./CountryListComponent";
-import DataDisplayComponent from "./DataDisplayComponent";
-import FeedComponent from "./FeedComponent";
-import Navbar from "react-bootstrap/Navbar";
-import LogoComponent from "./LogoComponent";
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import CountryListComponent from './CountryListComponent';
+import DataDisplayComponent from './DataDisplayComponent';
+import FeedComponent from './FeedComponent';
+import LogoComponent from './LogoComponent';
 import * as constants from '../server/constants';
-import * as api from '../../src/server/api';
+import * as api from '../server/api';
 
 class MainDashboardComponent extends React.Component {
-    constructor(props) {
+  constructor(props) {
     super(props);
-    this.state = {data: {}};
+    this.state = { data: {} };
   }
 
- async getData(options) {
-    var data;
-    await api.makeGetAPICall(options).then(response => { this.setState({data: response})});
-    return data;
-}
-    componentDidMount(){
-        var options = {
-    url: constants.URL.ALL_COUNTRIES,
-    requestType : constants.REQUEST_TYPE.GET
-}
-        this.getData(options);
-    }
-    render(){
+  componentDidMount() {
+    const options = {
+      url: constants.URL.ALL_COUNTRIES,
+      requestType: constants.REQUEST_TYPE.GET,
+    };
+    this.getData(options);
+  }
 
-    return <div className={"container-fluid"}>
-        <br/>
+  async getData(options) {
+    return api.makeGetAPICall(options).then((response) => {
+      this.setState({ data: response });
+    });
+  }
+
+
+  render() {
+    const { data } = this.state;
+    return (
+      // eslint-disable-next-line react/jsx-filename-extension
+      <div className="container-fluid">
+        <br />
         <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="#home">
-                <LogoComponent/> {'  '}
-                COVID-19 
-            </Navbar.Brand>
-        </Navbar><br/>
+          <Navbar.Brand href="#home">
+            <LogoComponent />
+            {' '}
+            {'  '}
+            COVID-19
+          </Navbar.Brand>
+        </Navbar>
+        <br />
         <div className="row">
-            <div className={"col-sm-2"}>
-                <CountryListComponent/>
-            </div>
-            <div className={"col-md-7"}>
-                <DataDisplayComponent/>
-            </div>
-            <div className={"col-md-3"}>
-                <FeedComponent/>
-            </div>
+          <div className="col-sm-2">
+            <CountryListComponent />
+          </div>
+          <div className="col-md-7">
+            <DataDisplayComponent data={data} />
+          </div>
+          <div className="col-md-3">
+            <FeedComponent />
+          </div>
         </div>
-    </div>
-    }
+      </div>
+    );
+  }
 }
 
 export default MainDashboardComponent;
--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
-    document.getElementById('root')
+  // eslint-disable-next-line react/jsx-filename-extension
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
